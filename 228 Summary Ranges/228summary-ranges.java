0bd48694dfe10a08aa86e00a5659692710a16b6e
Harshit0221 <harsh2003.it@gmail.com>
2025-12-06 09:44:05 +0000

    Code challenge submission for question: 228 Summary Ranges

---
+++ 228 Summary Ranges/228summary-ranges.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        ArrayList <String> result = new ArrayList <>();
+        int i = 0;
+        int n = nums.length;
+        while (i<n){
+            int start = nums[i];
+            while (i+1<n && nums[i+1]==nums[i]+1){
+                i++;
+            }
+            int end = nums[i];
+            if (start == end) result.add(Integer.toString(start));
+            else{
+                result.add(start + "->" + end);
+            }
+            i++;
+        }
+        return result;
+    }
+}
