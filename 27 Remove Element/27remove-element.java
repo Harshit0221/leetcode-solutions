9ebee2de53826b89911c05ced1252e9239838f3e
Harshit0221 <harsh2003.it@gmail.com>
2025-12-18 04:49:20 +0000

    Code challenge submission for question: 27 Remove Element

---
+++ 27 Remove Element/27remove-element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i=0;
+        int j=nums.length-1;
+        while (i<=j){
+            if (nums[i]==val){
+                swap (nums,i,j);
+                j--;
+            }else{
+                i++;
+            }
+        }
+        return i;
+    }
+    public void swap (int[]nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
